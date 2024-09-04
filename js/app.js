ca304ae94e0e2a9c89303550438b5cf4576d33f5
nichoOtero <nichonaks@gmail.com>
2024-09-03 22:33:03 -0500

    Initial html and CSS

---
+++ js/app.js
@@ -0,0 +1,51 @@
+/**
+1. Define any variables that are used to track the state of the game:
+ - Spaceman  image
+ - Target word
+ - Hint
+ - Player guess - letters
+ - Guess result - word includes/does not include letter
+ - Wrong gues count
+ - Game result - win/loss
+ - Result message displaying if player won or lost
+
+2. Define required constants:
+ - List or target words
+ - List of hints 
+ - Keyboard - letters player can choose
+
+
+3. Handle computer generating a random target word
+
+4. Handle computer generating a hint:
+
+5. Handle a player clicking a button
+
+6. Compare the player's guess to letters included in the target word
+
+7. Update the game board 
+ - If player’s guess is included in the target word, display that letter as many times as it appears in the word. 
+ - If player’s guess is not included in the target word, update the number of wrong guesses and the hangman image. 
+
+8. Render a win/lose message to the player 
+ - If player guesses all of the letters in the target word, display win message. If player reaches the limit of wrong guesses, display lose message
+
+9. Allow the player to reset the game and play again using a play again button
+ - Clear out the state of the game
+ - Reset game board and clear display messages.
+ */
+
+/*-------------- Constants -------------*/
+
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+
